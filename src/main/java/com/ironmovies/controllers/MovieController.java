abf10dd41ec04c263c026b2750ab1e16dec7b939
John Jastrow <jjastrow>
2017-08-10 12:31:11 -0500

    created some working tests

--- src/main/java/com/ironmovies/controllers/MovieController.java
+++ src/main/java/com/ironmovies/controllers/MovieController.java
@@ -2,9 +2,12 @@ package com.ironmovies.controllers;
 
 import com.ironmovies.models.Movie;
 import com.ironmovies.models.ResultsPage;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collector;
@@ -16,17 +19,34 @@ public class MovieController {
     List<Movie> movies = new ArrayList();
     static final String API_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=be2a38521a7859c95e2d73c48786e4bb";
 
+    @GetMapping("/")
+    public String home(){
+        return "You have reached the Movie REST API. Try: /api/...";
+    }
 
-    @GetMapping(path ="/api/test")
+    @GetMapping("/apitest")
+    public List<Movie> apiTest(){
+        Movie movie1 = new Movie();
+        movie1.setOverview("my overview");
+        movie1.setPopularity(99.0);
+        movie1.setPosterPath("https://yt3.ggpht.com/-Yai9Hqhjdl8/AAAAAAAAAAI/AAAAAAAAAAA/DdqlORbBBPQ/s900-c-k-no-mo-rj-c0xffffff/photo.jpg");
+        movie1.setTitle("my apiTest Title");
+        movies.add(movie1);
+        return movies;
+    }
+
+    @CrossOrigin(origins = "http://www.unrentedforest.surge.sh/")
+    @GetMapping(path ="/api/spider-homecoming")
     public Object singleMovieTest(String route) {
         RestTemplate testTemplate = new RestTemplate();
         ResultsPage resultsPage = testTemplate.getForObject(API_URL, ResultsPage.class);
         return resultsPage.getResults().stream()
                 .filter(e->e.getTitle()
                         .contains("Spider-Man: Homecoming"))
-                        .collect(Collectors.toList());
+                .collect(Collectors.toList());
     }
 
+    @CrossOrigin
     @GetMapping(path = "/api/movies")
     public Object getMovies(String route) {
         //TODO: checkfor api key ="abc"
--- src/test/java/com/ironmovies/MovieApplicationTests.java
+++ src/test/java/com/ironmovies/MovieApplicationTests.java
@@ -1,17 +1,48 @@
+package com.ironmovies;
 
+import static org.assertj.core.api.Assertions.assertThat;
 
+import com.ironmovies.models.Movie;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class MovieApplicationTests {
 
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
     @Test
-    public void contextLoads() {
+    public void greetingShouldReturnDefaultMessage() throws Exception {
+        String homeResult = "You have reached the Movie REST API";
+        String result = this.restTemplate.getForObject("http://localhost:" + port + "/",
+                String.class);
+        assertThat(result).contains(homeResult);
     }
+
+    @Test
+    public void apiTestTest() throws Exception {
+        List<Movie> result = this.restTemplate.getForObject("http://localhost:" + port + "/",
+                List.class);
+        Movie movieResult = result.get(0);
+        assertThat(result.size() > 0);
+        assertThat(movieResult.getTitle().equals("my apiTest Title"));
+    }
+
+}
 
 //    @Test
 //    public void testLargeNumbers () throws Exception {
@@ -19,6 +50,4 @@ public class MovieApplicationTests {
 //        assertTrue(MethodsClass.containsOne(10000002));
 //        assertFalse(MethodsClass.containsOne(986974958));
 //    }
-
-}
 
